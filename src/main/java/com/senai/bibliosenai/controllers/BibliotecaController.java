4df8667809388672df4616d1f364d6dba60a1a5d
PedroKich <pedrokich2@gmail.com>
2024-08-29 22:15:29 -0300

    coisas de hj :P

--- src/main/java/com/senai/bibliosenai/controllers/BibliotecaController.java
+++ src/main/java/com/senai/bibliosenai/controllers/BibliotecaController.java
@@ -1,5 +1,8 @@
 package com.senai.bibliosenai.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -7,7 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.senai.bibliosenai.dtos.request.CreateBookDTO;
+
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -17,6 +26,38 @@ public class BibliotecaController {
     @GetMapping("/biblioteca")
     public String findBiblioteca(){
         return "Teste";
+    }
+
+
+    // chat gpt fez isso
+
+    // @GetMapping("/image")
+    // public ResponseEntity<byte[]> getImage(@RequestParam("id") Long id) {
+    //     Image image = "yellowRabbit.png";
+
+    //     if (image == null) {
+    //         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    //     }
+
+    //     HttpHeaders headers = new HttpHeaders();
+    //     headers.set(HttpHeaders.CONTENT_TYPE, "image/png"); // ou outro tipo de imagem como "image/png"
+    //     headers.set(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + image.getName() + "\"");
+        
+    //     return new ResponseEntity<>(image.getContent(), headers, HttpStatus.OK);
+    // }
+
+
+    // chat gpt fez isso
+
+
+    @GetMapping("/yellowRabbit")
+    public String youCant(){
+        return "you can't🔪";
+    }
+
+    @GetMapping("/1985")
+    public String saveThem(){
+        return "S\b A\b V\b E\b T\b H\b E\b M\b";
     }
 
     @PostMapping("/livros")
@@ -38,5 +79,19 @@ public class BibliotecaController {
     public String alteraObjetoLivro(@PathVariable("id") int idLivro){
         return "Todos os campos alterados";
     }
+
+    @PostMapping
+    public ResponseEntity<?> postMethodName(@RequestBody CreateBookDTO dto) {
+
+        System.out.println(dto.getAuthor());
+
+        return ResponseEntity.status(201).build();
+        
+        
+    }
+    
+
+
+
 
 }
---
+++ src/main/java/com/senai/bibliosenai/dtos/request/CreateBookDTO.java
@@ -0,0 +1,43 @@
+package com.senai.bibliosenai.dtos.request;
+
+public class CreateBookDTO {
+    private String title;
+    private String author;
+    private int releaseYear;
+    private String description;
+    private String genre;
+    
+
+    public String getTitle() {
+        return title;   
+    }   
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getGenre() {
+        return genre;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+}
